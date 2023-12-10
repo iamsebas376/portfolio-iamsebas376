6c5e6f6bbd09b55d668b07fc7510bce6064c0be3
iamsebas376 <londonosebas201.sloa@gmail.com>
2023-12-10 17:32:12 -0500

    feat: add projects page

--- src/assets/data/testimonials.js
+++ src/assets/data/testimonials.js
@@ -11,14 +11,14 @@ const testimonials = [
         name: 'Camilo Alzate',
         title: 'SSr Back-End Java en Quipux, Desarrollador Full Stack',
         desc:
-            'Desde el comienzo al adentrarse en el mundo de la programación ha creado proyectos de alta calidad siempre buscando dar esa milla extra para el cumplimiento de cada uno de estos proyectos, buscando siempre las mejores prácticas, arquitecturas, diseños, eficacia y fluidez. Además de tener una excelente madurez profesional al momento de hacer proyectos en equipo y de forma individual, sin duda alguien que siempre dará todo y más para obtener un excelente resultado. ',
+            'Desde el comienzo al adentrarse en el mundo de la programación ha creado proyectos de alta calidad siempre buscando dar esa milla extra para el cumplimiento de cada uno de estos proyectos, buscando siempre las mejores prácticas, arquitecturas, diseños, eficacia y fluidez. Además de tener una excelente madurez profesional al momento de hacer proyectos en equipo y de forma individual, sin duda alguien que siempre dará todo y más para obtener un excelente resultado. ',
     },
     {
         id: 3,
         name: 'Juan Pavas',
-        title: 'Desarrollador Full Stack',
+        title: 'Desarrollador Senior Full Stack en Extiblu S.A.S',
         desc:
-            'Lorem ipsum dolor sit amet consectetur adipiscing, elit interdum eros cubilia turpis ad sed, dui justo rutrum magnis elementum. ',
+            'Desde sus primeros pasos hasta su inmersión en el fascinante mundo del desarrollo, ha demostrado una habilidad excepcional para crear proyectos de alta calidad, su compromiso con la excelencia y su constante búsqueda de la perfección se reflejan claramente en cada una de sus creaciones, destaca por su madurez profesional. Estoy seguro de que continuará superando expectativas y alcanzando metas aún más grandes en su carrera profesional tanto en proyectos colaborativos como en tareas individuales.',
     },
 ];
 export default testimonials;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,100 @@
 import React from "react";
+import SectionTitle from "../components/SectionTitle";
+import ProjectItem from "../components/ProjectItem";
+import styled from "styled-components";
+import { MdSearch } from "react-icons/md";
+import ProjectInfo from "../assets/data/projects";
+
+const ProjectStyles = styled.div`
+  padding: 10rem 0;
+  .projects-all-items {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    gap: 5rem;
+    margin-top: 5rem;
+  }
+  .projects-search-bar {
+    position: relative;
+    width: 300px;
+    margin-top: 5rem;
+  }
+  .projects-search-bar input {
+    width: 100%;
+    font-size: 2rem;
+    padding: 0.8rem;
+    color: var(--black);
+    border-radius: 6px;
+    outline: none;
+    border: none;
+  }
+  .projects-search-bar .search-icon {
+    position: absolute;
+    width: 2rem;
+
+    right: 1rem;
+  }
+  .projects-search-bar .search-icon path {
+    color: var(--deep-dark);
+  }
+  @media only screen and (max-width: 768px) {
+    .projects-search-bar,
+    .projects-search-bar form,
+    .projects-search-bar input {
+      width: 100%;
+    }
+  }
+`;
 
 function Projects() {
+  const [searchText, setSearchText] = React.useState("");
+  const [projectData, setProjectData] = React.useState(ProjectInfo);
+
+  React.useEffect(() => {
+    if (searchText === "") return;
+    setProjectData(() =>
+      ProjectInfo.filter(
+        (item) =>
+          item.title &&
+          item.title.toLowerCase().includes(searchText.toLowerCase())
+      )
+    );
+  }, [searchText]);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchText(e.target.value);
+    if (!e.target.value.length > 0) {
+      setProjectData(ProjectInfo);
+    }
+  };
+
   return (
-    <div>
-      <h1> Projects</h1>
-    </div>
+    <ProjectStyles>
+      <div className="container">
+        <SectionTitle heading="Proyectos" subHeading="Algunos de mis" />
+        <div className="projects-search-bar">
+          <form>
+            <input
+              type="text"
+              value={searchText}
+              onChange={handleChange}
+              placeholder="Nombre del proyecto"
+            />
+            <MdSearch className="search-icon" />
+          </form>
+        </div>
+        <div className="projects-all-items">
+          {projectData.map((item) => (
+            <ProjectItem
+              key={item.id}
+              title={item.title}
+              desc={item.desc}
+              img={item.img}
+            />
+          ))}
+        </div>
+      </div>
+    </ProjectStyles>
   );
 }
 
